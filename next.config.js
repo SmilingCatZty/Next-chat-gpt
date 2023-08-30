8420dc5e3dc77094e500d0823e401b3b6ec3ffa3
ZhouTianyang <SmilingCat.tyz@gmail.com>
2023-08-30 14:54:59 +0800

    feat: 新建next项目,完成基础配置

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** 
+ * Next配置文件
+ * @type {import('next').NextConfig} 
+ */
+const nextConfig = {
+  basePath: '/',
+  env: {
+    customKey: 'my-value',
+  },
+  eslint: {},
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
